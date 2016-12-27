c1360a461ca882aff157afdb269344fdeff640ca
Mike Roylance <roylance.michael@gmail.com>
2016-12-27 09:47:04 -0700

    updated dependencies

--- ui/src/main/java/org/roylance/yaclib/enums/UITokens.kt
+++ ui/src/main/java/org/roylance/yaclib/enums/UITokens.kt
@@ -2,7 +2,7 @@ package org.roylance.yaclib.enums
 
 object UITokens {
     const val YaclibMajor = 0
-    const val YaclibMinor = 195
+    const val YaclibMinor = 196
     const val YaclibGroup = "org.roylance.yaclib"
     const val YaclibName = "yaclib"
 
