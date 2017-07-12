ca27cf4bc934c81493bb523eda54c33bb6e924b1
Mike Roylance <roylance.michael@gmail.com>
2017-07-12 08:11:49 -0600

    Updated dependencies

--- core/src/main/java/org/roylance/yaclib/core/utilities/JavaUtilities.kt
+++ core/src/main/java/org/roylance/yaclib/core/utilities/JavaUtilities.kt
@@ -30,12 +30,12 @@ object JavaUtilities {
 
     const val ArtifactoryVersion = "4.4.0"
     const val BintrayVersion = "1.7"
-    const val RoylanceCommonVersion = "0.18"
+    const val RoylanceCommonVersion = "0.20"
     const val TomcatVersion = "8.0.28"
-    const val KotlinVersion = "1.1.3"
+    const val KotlinVersion = "1.1.3-2"
 
     const val CommonsIOVersion = "2.5"
-    const val ProtobufVersion = "3.2.0"
+    const val ProtobufVersion = "3.3.0"
     const val HttpComponentsVersion = "4.5.1"
     const val IntellijAnnotationsVersion = "12.0"
     const val JerseyJsonVersion = "1.19.1"
